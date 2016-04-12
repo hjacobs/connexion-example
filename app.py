270cf0d265d14cd2e1db74a030dea5ec2ed2f62f
Arjun Naik <arjun.naik@zalando.de>
2016-04-12 15:02:20 +0200

    Update pet. No overwrite of created date.

--- app.py
+++ app.py
@@ -23,10 +23,11 @@ def put_pet(pet_id, pet):
     pet['id'] = pet_id
     if exists:
         logging.info('Updating pet %s..', pet_id)
+        PETS[pet_id].update(pet)
     else:
         logging.info('Creating pet %s..', pet_id)
         pet['created'] = datetime.datetime.utcnow()
-    PETS[pet_id] = pet
+        PETS[pet_id] = pet
     return NoContent, (200 if exists else 201)
 
 
